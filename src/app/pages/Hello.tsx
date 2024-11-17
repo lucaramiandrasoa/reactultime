558b61a0f204ed3bd168fd693eb947f4ba3ad6c3
Lukas <ramiandrasoaluca@gmail.com>
2024-11-17 18:49:50 +0300

    Last commit

---
+++ src/app/pages/Hello.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
+const Hello = () => {
+  // const nom="RAKOTO";
+  return (
+    <>
+      <div>Hello</div>
+
+      <div className='bg-red-600 rounded'>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent >
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </>
+  )
+}
+
+
+export default Hello
+
+
